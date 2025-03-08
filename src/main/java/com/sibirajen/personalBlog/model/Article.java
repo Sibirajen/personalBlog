78ad2a526e401f279eba1692531250a7030bab44
sibirajen <sibirajen16@gmail.com>
2025-03-08 11:10:39 +0530

    Added Article.java

---
+++ src/main/java/com/sibirajen/personalBlog/model/Article.java
@@ -0,0 +1,13 @@
+package com.sibirajen.personalBlog.model;
+
+import java.time.LocalDate;
+
+public class Article {
+    private String id;
+    private String title;
+    private String content;
+    private LocalDate createDate;
+    private LocalDate updateDate;
+
+
+}
