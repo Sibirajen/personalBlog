22b0570ef6e717c4147f89dcd036935969b91eba
sibirajen <sibirajen16@gmail.com>
2025-03-08 18:07:44 +0530

    Added lombok functionalities

--- src/main/java/com/sibirajen/personalBlog/model/Article.java
+++ src/main/java/com/sibirajen/personalBlog/model/Article.java
@@ -1,13 +1,17 @@
 package com.sibirajen.personalBlog.model;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Article {
     private String id;
     private String title;
     private String content;
     private LocalDate createDate;
     private LocalDate updateDate;
-
-
 }
--- src/main/java/com/sibirajen/personalBlog/model/User.java
+++ src/main/java/com/sibirajen/personalBlog/model/User.java
@@ -1,7 +1,15 @@
 package com.sibirajen.personalBlog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
     private String name;
     private String password;
-
 }
