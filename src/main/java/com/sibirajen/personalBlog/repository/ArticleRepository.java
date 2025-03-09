0a944407124dc5be960ae78032395400c13a9b77
sibirajen <sibirajen16@gmail.com>
2025-03-09 13:18:35 +0530

    Added Constructor for Database initialization

--- src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
+++ src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
@@ -4,5 +4,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class ArticleRepository {
+    private final JsonFileDatabase database;
 
+    public ArticleRepository() {
+        this.database = new JsonFileDatabase("/articles");
+    }
 }
