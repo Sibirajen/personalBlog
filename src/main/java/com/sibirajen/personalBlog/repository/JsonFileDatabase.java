a53e52f4a4a56822fd5daca814899f33d5b3ae3a
sibirajen <sibirajen16@gmail.com>
2025-03-09 13:18:59 +0530

    Added read and write functionality

---
+++ src/main/java/com/sibirajen/personalBlog/repository/JsonFileDatabase.java
@@ -0,0 +1,42 @@
+package com.sibirajen.personalBlog.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JsonFileDatabase {
+    private final ObjectMapper objectMapper;
+    private final Path DATABASE_PATH;
+
+    public JsonFileDatabase(String path) {
+        this.objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        this.DATABASE_PATH = Path.of("src/main/java/com/sibirajen/personalBlog/data", path);
+    }
+
+    public <T> void write(T bean, String id) throws IOException {
+        Path filePath = DATABASE_PATH.resolve( "article_" + id + ".json");
+        Files.createDirectories(DATABASE_PATH);
+        objectMapper.writeValue(filePath.toFile(), bean);
+    }
+
+    public <T> List<T> read(Class<T> clazz) throws IOException {
+        try (Stream<Path> paths = Files.list(DATABASE_PATH)) {
+            return paths
+                    .filter( path -> path.toString().endsWith(".json"))
+                    .map( path -> {
+                        try {
+                            return objectMapper.readValue(path.toFile(), clazz);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.toList());
+        }
+    }
+}
