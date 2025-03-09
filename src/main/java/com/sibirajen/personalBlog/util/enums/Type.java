024fadf6d15e8a6ad7b9668c017911f07c523f2e
sibirajen <sibirajen16@gmail.com>
2025-03-09 15:19:38 +0530

    Added @Getter and made string final

--- src/main/java/com/sibirajen/personalBlog/util/enums/Type.java
+++ src/main/java/com/sibirajen/personalBlog/util/enums/Type.java
@@ -1,16 +1,16 @@
 package com.sibirajen.personalBlog.util.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Type {
     ARTICLE("article_"),
     USER("user_");
 
-    private String string;
+    private final String string;
 
     Type(String string) {
         this.string = string;
     }
 
-    public String getString() {
-        return string;
-    }
 }
