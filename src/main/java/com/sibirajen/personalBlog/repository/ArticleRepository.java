40424d627bcb51cefa2ddcedd838baa84a91abc0
sibirajen <sibirajen16@gmail.com>
2025-03-09 15:21:36 +0530

    Added getFileId method

--- src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
+++ src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
@@ -1,12 +1,21 @@
 package com.sibirajen.personalBlog.repository;
 
+import com.sibirajen.personalBlog.util.GenerateFileID;
+import com.sibirajen.personalBlog.util.enums.Type;
 import org.springframework.stereotype.Repository;
+
+import java.nio.file.Path;
 
 @Repository
 public class ArticleRepository {
     private final JsonFileDatabase database;
+    private static final Path articlePath = Path.of("/articles");
 
     public ArticleRepository() {
-        this.database = new JsonFileDatabase("/articles");
+        this.database = new JsonFileDatabase(articlePath.toString());
+    }
+
+    public static String getFileId(String id){
+        return GenerateFileID.getId(id, Type.ARTICLE);
     }
 }
