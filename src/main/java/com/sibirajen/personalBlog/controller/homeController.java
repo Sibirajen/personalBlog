b4e307d9316b957eafd629ce4d30d2853d5195f7
sibirajen <sibirajen16@gmail.com>
2025-03-20 15:46:06 +0530

    Added homeController for mapping /home

--- src/main/java/com/sibirajen/personalBlog/controller/homeController.java
+++ src/main/java/com/sibirajen/personalBlog/controller/homeController.java
@@ -1,4 +1,12 @@
 package com.sibirajen.personalBlog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class homeController {
+    @GetMapping("/home")
+    public String getHome(){
+        return "home";
+    }
 }
