842d070b0ff20d7f793f873f28e84b99d1f59753
sibirajen <sibirajen16@gmail.com>
2025-03-29 10:23:03 +0530

    Added ArticleController

---
+++ src/main/java/com/sibirajen/personalBlog/controller/ArticleController.java
@@ -0,0 +1,28 @@
+package com.sibirajen.personalBlog.controller;
+
+import com.sibirajen.personalBlog.model.Article;
+import com.sibirajen.personalBlog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ArticleController {
+
+    private final ArticleService service;
+
+    @Autowired
+    public ArticleController(ArticleService service){
+        this.service = service;
+    }
+
+    @GetMapping("/article/{id}")
+    public String getArticle(@PathVariable String id, Model model){
+        Article article = service.getArticle(id);
+        model.addAttribute("article", article);
+
+        return "article";
+    }
+}
