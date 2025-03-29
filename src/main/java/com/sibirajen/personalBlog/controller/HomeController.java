beb4f09b83f71c321cf1c61b70b2d7f075cafe72
sibirajen <sibirajen16@gmail.com>
2025-03-29 10:15:28 +0530

    Renamed from homeController.java to HomeController.java

---
+++ src/main/java/com/sibirajen/personalBlog/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.sibirajen.personalBlog.controller;
+
+import com.sibirajen.personalBlog.model.Article;
+import com.sibirajen.personalBlog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final ArticleService service;
+
+    @Autowired
+    public HomeController(ArticleService service){
+        this.service = service;
+    }
+
+    @GetMapping(path = "/home")
+    public String getHome(Model model){
+
+        List<Article> articles = service.getAllArticles();
+
+        model.addAttribute("articles", articles);
+        return "home";
+
+//        Entertainment
+    }
+}
