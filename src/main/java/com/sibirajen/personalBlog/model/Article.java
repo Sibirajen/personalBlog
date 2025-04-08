b6c3b3579c6928fd66fd1d340fa7162deb1e8b51
sibirajen <sibirajen16@gmail.com>
2025-04-08 12:23:16 +0530

    Added necessary fields

--- src/main/java/com/sibirajen/personalBlog/model/Article.java
+++ src/main/java/com/sibirajen/personalBlog/model/Article.java
@@ -1,6 +1,6 @@
 package com.sibirajen.personalBlog.model;
 
-import com.sibirajen.personalBlog.util.ShortIdGen;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -8,22 +8,39 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
+@Entity
+@Table(name = "ARTICLES")
+@Access(AccessType.FIELD)
 public class Article {
-    private String articleId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ARTICLE_ID")
+    private Long articleId;
+
+    @Column(name = "AUTHOR_NAME")
     private String authorName;
+
+    @Column(name = "AUTHOR_ID")
     private String authorId;
+
+    @Column(name = "TITLE")
     private String title;
+
+    @Column(name = "CONTENT" ,columnDefinition = "TEXT")
     private String content;
+
+    @Column(name = "CREATE_DATE" ,updatable = false, nullable = false)
     private LocalDate createDate;
 
-    @Builder
-    public Article(String authorName, String authorId, String title, String content, LocalDate createDate) {
-        this.articleId = ShortIdGen.getID();
+    public Article(String authorName, String authorId, String title, String content) {
         this.authorName = authorName;
         this.authorId = authorId;
         this.title = title;
         this.content = content;
-        this.createDate = createDate;
+    }
+
+    @PrePersist
+    public void onCreate(){
+        this.createDate = LocalDate.now();
     }
 }
