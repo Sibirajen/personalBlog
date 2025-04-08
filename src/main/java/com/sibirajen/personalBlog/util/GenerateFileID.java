8c9cd357f5e2a0a094624a21d17fa7b37fa99d33
sibirajen <sibirajen16@gmail.com>
2025-04-08 12:24:06 +0530

    Moved to using Db instead of fileDb

--- src/main/java/com/sibirajen/personalBlog/util/GenerateFileID.java
+++
@@ -1,9 +0,0 @@
-package com.sibirajen.personalBlog.util;
-
-import com.sibirajen.personalBlog.util.enums.Type;
-
-public class GenerateFileID {
-    public static String getId(String id, Type type){
-        return type.getString() + id;
-    }
-}
--- src/main/java/com/sibirajen/personalBlog/util/ShortIdGen.java
+++
@@ -1,7 +0,0 @@
-package com.sibirajen.personalBlog.util;
-
-public class ShortIdGen {
-    public static String getID() {
-        return Long.toHexString(System.nanoTime());
-    }
-}
