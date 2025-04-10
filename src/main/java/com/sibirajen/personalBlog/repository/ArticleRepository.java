e1257108c42d7c996572b9368616ab3b5d9092e1
sibirajen <sibirajen16@gmail.com>
2025-04-10 08:44:19 +0530

    Added Article repo

--- src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
+++ src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
@@ -1,48 +1,10 @@
 package com.sibirajen.personalBlog.repository;
 
 import com.sibirajen.personalBlog.model.Article;
-import com.sibirajen.personalBlog.util.GenerateFileID;
-import com.sibirajen.personalBlog.util.enums.Type;
 import org.springframework.stereotype.Repository;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public class ArticleRepository {
-    private final JsonFileDatabase<Article> database;
-    private static final Path articlePath = Path.of("/articles");
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-    public ArticleRepository() {
-        this.database = new JsonFileDatabase<>(articlePath.toString());
-    }
-
-    public static String getFileId(String id){
-        return GenerateFileID.getId(id, Type.ARTICLE);
-    }
-
-    public Article getArticleById(String id){
-        try {
-            return database.readFile(id , Article.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void save(Article article){
-        try {
-            database.writeFile(article.getArticleId(), article);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<Article> getArticles() {
-        try {
-            return database.readAllFiles(Article.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
