73c22202667c6efbb65c68cb0bd29ba487f1825b
sibirajen <sibirajen16@gmail.com>
2025-05-02 20:28:20 +0530

    DTO Class for publishing an article

---
+++ src/main/java/com/sibirajen/personalBlog/model/ArticleRequest.java
@@ -0,0 +1,13 @@
+package com.sibirajen.personalBlog.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ArticleRequest {
+    private String title;
+    private String content;
+    private String authorName;
+}
