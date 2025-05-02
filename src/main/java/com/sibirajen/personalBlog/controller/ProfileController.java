467641adaa7b683116d8694b713f2f9e439b3b05
sibirajen <sibirajen16@gmail.com>
2025-05-02 20:28:46 +0530

    /publish endpoint created

---
+++ src/main/java/com/sibirajen/personalBlog/controller/ProfileController.java
@@ -0,0 +1,31 @@
+package com.sibirajen.personalBlog.controller;
+
+import com.sibirajen.personalBlog.model.ArticleRequest;
+import com.sibirajen.personalBlog.service.ProfileService;
+import com.sibirajen.personalBlog.util.SessionValidator;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller("/profile")
+public class ProfileController {
+    private final ProfileService service;
+
+    @Autowired
+    public ProfileController(ProfileService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/profile/publish")
+    public String postArticle(HttpSession session,
+                              @ModelAttribute ArticleRequest articleRequest) {
+        if (!SessionValidator.isSessionValid(session))
+            return "redirect:/login";
+
+        String email = (String) session.getAttribute("email");
+        service.postArticle(articleRequest, email);
+        return "error";
+    }
+}
