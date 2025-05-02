1c264f9fccb0cd2f463fea75b9c5703698734d48
sibirajen <sibirajen16@gmail.com>
2025-05-02 20:29:00 +0530

    service for publishController

---
+++ src/main/java/com/sibirajen/personalBlog/service/ProfileService.java
@@ -0,0 +1,43 @@
+package com.sibirajen.personalBlog.service;
+
+import com.sibirajen.personalBlog.model.Article;
+import com.sibirajen.personalBlog.model.ArticleRequest;
+import com.sibirajen.personalBlog.model.User;
+import com.sibirajen.personalBlog.model.builder.ArticleBuilder;
+import com.sibirajen.personalBlog.repository.ArticleRepository;
+import com.sibirajen.personalBlog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProfileService {
+    private final UserRepository userRepo;
+    private final ArticleRepository articleRepo;
+
+    @Autowired
+    public ProfileService(UserRepository userRepo, ArticleRepository articleRepo) {
+        this.userRepo = userRepo;
+        this.articleRepo = articleRepo;
+    }
+
+    public void postArticle(ArticleRequest articleRequest, String emailId) {
+        String normalizedEmail = emailId.toLowerCase();
+
+        Optional<User> user = userRepo.findByEmailId(emailId);
+
+        if(user.isEmpty()){
+            return;
+        }
+
+        Article article = ArticleBuilder.builder()
+                .title(articleRequest.getTitle())
+                .authorName(articleRequest.getAuthorName())
+                .author(user.get())
+                .content(articleRequest.getContent())
+                .build();
+
+        articleRepo.save(article);
+    }
+}
