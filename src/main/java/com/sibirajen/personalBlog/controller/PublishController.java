0d5e13dbd161800f720d0bb4337d3eff33e0fe5e
sibirajen <sibirajen16@gmail.com>
2025-05-06 15:41:58 +0530

    renamed and made to have get endpoint to publish

--- src/main/java/com/sibirajen/personalBlog/controller/PublishController.java
+++ src/main/java/com/sibirajen/personalBlog/controller/PublishController.java
@@ -1,24 +1,36 @@
 package com.sibirajen.personalBlog.controller;
 
 import com.sibirajen.personalBlog.model.ArticleRequest;
-import com.sibirajen.personalBlog.service.ProfileService;
+import com.sibirajen.personalBlog.service.PublishService;
 import com.sibirajen.personalBlog.util.SessionValidator;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
-@Controller("/profile")
-public class ProfileController {
-    private final ProfileService service;
+@Controller
+public class PublishController {
+    private final PublishService service;
 
     @Autowired
-    public ProfileController(ProfileService service) {
+    public PublishController(PublishService service) {
         this.service = service;
     }
 
-    @PostMapping("/profile/publish")
+    @GetMapping("/publish")
+    public String getArticle(HttpSession session, Model model){
+        if (!SessionValidator.isSessionValid(session))
+            return "redirect:/login";
+
+        model.addAttribute("logged", true);
+        return "publish";
+    }
+
+    @PostMapping("/publish")
     public String postArticle(HttpSession session,
                               @ModelAttribute ArticleRequest articleRequest) {
         if (!SessionValidator.isSessionValid(session))
