8f8ffd7fa195bcdfc8a77e8b64e5da938b6a15c4
sibirajen <sibirajen16@gmail.com>
2025-05-15 11:52:31 +0530

    added findByAuthorId method to this repo

--- src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
+++ src/main/java/com/sibirajen/personalBlog/repository/ArticleRepository.java
@@ -4,7 +4,9 @@ import com.sibirajen.personalBlog.model.Article;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+    List<Article> findByAuthorId(Long authorId);
 }
